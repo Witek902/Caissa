597d56d430184aca8cc84c516f8c099c67c1295a
Michal Witanowski <witek902@gmail.com>
2021-11-12 01:28:53 +0100

    Bump version to 0.4

--- src/frontend/UCI.cpp
+++ src/frontend/UCI.cpp
@@ -11,7 +11,7 @@
 #define NOMINMAX
 #include <Windows.h>
 
-static const char* c_EngineName = "Caissa 0.3";
+static const char* c_EngineName = "Caissa 0.4";
 static const char* c_Author = "Michal Witanowski";
 
 // TODO set TT size based on current memory usage / total memory size
