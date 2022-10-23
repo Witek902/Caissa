ba89c709a4ed65e8c4f43e4b2ad692a84c7f9943
Michal Witanowski <witek902@gmail.com>
2022-10-24 00:25:33 +0200

    Bump to version 1.2

--- src/frontend/UCI.cpp
+++ src/frontend/UCI.cpp
@@ -10,7 +10,7 @@
 #include <math.h>
 #include <random>
 
-#define VersionNumber "1.1.13"
+#define VersionNumber "1.2"
 
 #if defined(USE_BMI2) && defined(USE_AVX2) 
 #define ArchitectureStr "AVX2/BMI2"
