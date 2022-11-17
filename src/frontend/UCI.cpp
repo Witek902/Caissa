9ad4235dd3542c81b89b1626b1aae100a1276291
Michal Witanowski <witek902@gmail.com>
2022-11-17 02:52:07 +0100

    Search: Tweak singular extensions and late move reductions
    - Allow for reducing late good captures

--- src/frontend/UCI.cpp
+++ src/frontend/UCI.cpp
@@ -10,7 +10,7 @@
 #include <math.h>
 #include <random>
 
-#define VersionNumber "1.3.1"
+#define VersionNumber "1.3.2"
 
 #if defined(USE_BMI2) && defined(USE_AVX2) 
 #define ArchitectureStr "AVX2/BMI2"
