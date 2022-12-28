0453b6660bd7e72e2076c70f0670a89ad5510c64
Michal Witanowski <witek902@gmail.com>
2022-12-28 23:31:10 +0100

    Better KPPvK endgame evaluation

--- src/frontend/UCI.cpp
+++ src/frontend/UCI.cpp
@@ -11,7 +11,7 @@
 #include <random>
 #include <atomic>
 
-#define VersionNumber "1.4.7"
+#define VersionNumber "1.4.8"
 
 #if defined(USE_BMI2) && defined(USE_AVX2) 
 #define ArchitectureStr "AVX2/BMI2"
