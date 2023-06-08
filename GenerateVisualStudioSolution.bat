341a82cea40ab0c7ab8e9e3affaffaff7d004ca6
Michal Witanowski <witek902@gmail.com>
2023-06-09 00:52:03 +0200

    Bump to version 1.9

--- GenerateVisualStudioSolution.bat
+++ GenerateVisualStudioSolution.bat
@@ -1,1 +1,4 @@
-cmake . -B build -G "Visual Studio 17 2022" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DTARGET_ARCH=x64-avx2-bmi2
+cmake . -B build_avx512 -G "Visual Studio 17 2022" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DTARGET_ARCH=x64-avx512
+cmake . -B build_avx2 -G "Visual Studio 17 2022" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DTARGET_ARCH=x64-avx2-bmi2
+cmake . -B build_sse2 -G "Visual Studio 17 2022" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DTARGET_ARCH=x64-sse4-popcnt
+cmake . -B build_legacy -G "Visual Studio 17 2022" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DTARGET_ARCH=x64-legacy
