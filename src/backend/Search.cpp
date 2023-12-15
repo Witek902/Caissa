2c28b64a129b68b05773392b58c7ea701391beb1
Michal Witanowski <witek902@gmail.com>
2023-12-15 22:03:24 +0100

    Search: RFP on invalid TT move
    Bench: 5774009

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1587,6 +1587,10 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
         isImproving = evalImprovement >= 0;
     }
 
+    const Move pvMove = thread.GetPvMove(*node);
+    const PackedMove ttMove = ttEntry.move.IsValid() ? ttEntry.move : pvMove;
+    const bool ttCapture = ttMove.IsValid() && (position.IsCapture(ttMove) || ttMove.GetPromoteTo() != Piece::None);
+
     if constexpr (!isPvNode)
     {
         if (!node->filteredMove.IsValid() && !node->isInCheck)
@@ -1595,7 +1599,8 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
             if (node->depth <= BetaPruningDepth &&
                 eval <= KnownWinValue &&
                 eval >= beta &&
-                eval >= (beta + BetaMarginBias + BetaMarginMultiplier * (node->depth - isImproving)))
+                eval >= (beta + BetaMarginBias + BetaMarginMultiplier * (node->depth - isImproving)) &&
+                (!ttMove.IsValid() || ttCapture))
             {
 #ifdef ENABLE_SEARCH_TRACE
                 trace.OnNodeExit(SearchTrace::ExitReason::BetaPruning, alpha);
@@ -1761,11 +1766,6 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
     {
         extension++;
     }
-
-    const Move pvMove = thread.GetPvMove(*node);
-    const PackedMove ttMove = ttEntry.move.IsValid() ? ttEntry.move : pvMove;
-
-    const bool ttCapture = ttMove.IsValid() && (position.IsCapture(ttMove) || ttMove.GetPromoteTo() != Piece::None);
 
     thread.moveOrderer.InitContinuationHistoryPointers(*node);
 
