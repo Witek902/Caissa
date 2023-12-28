103840988525aaf0d6b6d9590a1dd16c11492814
Michal Witanowski <witek902@gmail.com>
2023-12-28 12:03:41 +0100

    New neural net
    Elo   | 2.34 +- 2.06 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=32MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 51766 W: 12495 L: 12147 D: 27124
    Penta | [226, 6066, 12951, 6414, 226]
    
    Bench: 5705199

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-24.pnn";
+    const char* c_DefaultEvalFile = "eval-28.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
