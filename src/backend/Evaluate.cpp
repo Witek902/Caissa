6f27513fccc4fce823a730e7053b7ba18d61064a
Michal Witanowski <witek902@gmail.com>
2023-12-31 00:51:26 +0100

    New net
    Elo   | 1.49 +- 1.50 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 97290 W: 23189 L: 22772 D: 51329
    Penta | [440, 11494, 24395, 11841, 475]
    
    Bench: 5638113

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-28.pnn";
+    const char* c_DefaultEvalFile = "eval-29.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
