c8978e94332217d39e43afe8f3619e112a971c60
Michal Witanowski <witek902@gmail.com>
2024-01-30 16:27:30 +0100

    Search: More double extensions
    Bench 5082404

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -59,7 +59,7 @@ DEFINE_PARAM(AspirationWindowMaxSize, 498, 200, 1000);
 DEFINE_PARAM(AspirationWindow, 10, 6, 20);
 
 DEFINE_PARAM(SingularExtensionMinDepth, 5, 4, 10);
-DEFINE_PARAM(SingularDoubleExtensionMarigin, 18, 10, 30);
+DEFINE_PARAM(SingularDoubleExtensionMarigin, 8, 2, 20);
 
 DEFINE_PARAM(QSearchFutilityPruningOffset, 100, 50, 150);
 
@@ -1885,6 +1885,7 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
                 node->alpha = singularBeta - 1;
                 node->beta = singularBeta;
                 node->filteredMove = move;
+
                 const ScoreType singularScore = NegaMax<NodeType::NonPV>(thread, node, ctx);
 
                 // restore node state
@@ -1901,7 +1902,7 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
                         moveExtension = 1;
                         // double extension if singular score is way below beta
                         if constexpr (!isPvNode)
-                            if (node->doubleExtensions <= 6 && singularScore < singularBeta - SingularDoubleExtensionMarigin)
+                            if (node->doubleExtensions <= 10 && singularScore < singularBeta - SingularDoubleExtensionMarigin)
                                 moveExtension = 2;
                     }
                 }
