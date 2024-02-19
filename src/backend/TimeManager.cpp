0769e96b5c4b582698a955177d2bbee204eb753d
Michal Witanowski <witek902@gmail.com>
2024-02-19 16:52:31 +0100

    Time manager tweak
    Bench 6275385

--- src/backend/TimeManager.cpp
+++ src/backend/TimeManager.cpp
@@ -13,8 +13,8 @@ DEFINE_PARAM(TM_NodesCountScale, 199, 160, 260);
 DEFINE_PARAM(TM_NodesCountOffset, 53, 10, 90);
 DEFINE_PARAM(TM_StabilityScale, 37, 0, 200);
 DEFINE_PARAM(TM_StabilityOffset, 1254, 1000, 2000);
-DEFINE_PARAM(TM_PredictedMoveHitScale, 900, 800, 1000);
-DEFINE_PARAM(TM_PredictedMoveMissScale, 1100, 1000, 1500);
+DEFINE_PARAM(TM_PredictedMoveHitScale, 906, 800, 1000);
+DEFINE_PARAM(TM_PredictedMoveMissScale, 1136, 1000, 1500);
 
 static float EstimateMovesLeft(const uint32_t moves)
 {
