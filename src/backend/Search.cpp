dde420c4bde7e051166cf0d728db43402e24c81b
Michal Witanowski <witek902@gmail.com>
2024-02-23 01:54:53 +0100

    Search: Use best move from singular search as second move to search
    Bench 6214413

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1900,7 +1900,11 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
                 node->alpha = singularBeta - 1;
                 node->beta = singularBeta;
                 node->filteredMove = move;
+                node->bestMove = Move::Invalid();
                 const ScoreType singularScore = NegaMax<NodeType::NonPV>(thread, node, ctx);
+
+                // use the best move from singular search as a next move to be searched
+                movePicker.SetSecondMove(node->bestMove);
 
                 // restore node state
                 node->isPvNodeFromPrevIteration = originalIsPvNodeFromPrevIteration;
@@ -2227,5 +2231,6 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
         }
     }
 
+    node->bestMove = bestMove;
     return bestValue;
 }
