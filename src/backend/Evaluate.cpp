d3ff53ac0ea8b7086e33161911cca4bea750839e
Michal Witanowski <witek902@gmail.com>
2024-02-26 12:33:01 +0100

    New net
    Finetuning previous net (eval-33) for 19B iterations.
    Starting LR: 1e-4
    Final LR: 1e-5
    Batch size: 32K
    Dataset size: 7.46B
    
    Elo   | 3.94 +- 2.86 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=8MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 27154 W: 6653 L: 6345 D: 14156
    Penta | [92, 3117, 6877, 3373, 118]
    
    Elo   | 5.33 +- 4.07 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 12976 W: 3118 L: 2919 D: 6939
    Penta | [11, 1475, 3323, 1662, 17]
    
    DFRC.epd
    Elo   | 3.28 +- 2.46 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.48 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 28108 W: 5296 L: 5031 D: 17781
    Penta | [191, 2848, 7739, 3057, 219]
    
    Bench 6070781

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-33.pnn";
+    const char* c_DefaultEvalFile = "eval-34.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
