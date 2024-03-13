e94743ae804f58d2b4d8109af0939d3edeeb691d
Michal Witanowski <witek902@gmail.com>
2024-03-13 23:40:38 +0100

    New net
    Finetuning previous net (eval-34) with more data (8.1B positions).
    - LR = 1e-4 (24B iterations)
    - LR = 1e-5 (20B iterations)
    - batch size = 32K
    
    Book=UHO_Lichess_4852_v1.epd
    Elo   | 3.09 +- 2.67 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 2.50]
    Games | N: 30440 W: 7275 L: 7004 D: 16161
    Penta | [100, 3582, 7592, 3839, 107]
    
    Book=DFRC.epd
    Elo   | 3.17 +- 2.40 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 2.50]
    Games | N: 29282 W: 5489 L: 5222 D: 18571
    Penta | [211, 2918, 8158, 3101, 253]
    
    Bench 6619085

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-34.pnn";
+    const char* c_DefaultEvalFile = "eval-35.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
