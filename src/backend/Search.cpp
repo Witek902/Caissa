2fd99a7847725e2d6f13501bef99bb0ae098134b
Michal Witanowski <witek902@gmail.com>
2024-03-22 01:05:57 +0100

    Search: More history pruning
    Bench 9150961

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1774,12 +1774,13 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
 
                     continue;
                 }
+            }
 
+            if (move.IsQuiet())
+            {
                 // History Pruning
                 // if a move score is really bad, do not consider this move at low depth
-                if (quietMoveIndex > 1 &&
-                    node->depth < 9 &&
-                    moveStatScore < GetHistoryPruningTreshold(node->depth))
+                if (moveStatScore < GetHistoryPruningTreshold(node->depth))
                 {
                     continue;
                 }
