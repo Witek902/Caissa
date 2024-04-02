2881b0f8915cc184fd2cf80efd2ebb4d31797e87
Michal Witanowski <witek902@gmail.com>
2024-04-02 13:56:12 +0200

    New neural net
    Finetuning previous net with 9.28B positions
    (added 1B positions from regular chess games)
    
    First stage: LR=2.0e-4, 27B iterations
    Second stage: LR=1.0e-5, 24B iterations
    
    Elo   | 8.46 +- 4.29 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.94 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 12114 W: 3064 L: 2769 D: 6281
    Penta | [62, 1369, 2895, 1674, 57]
    
    Elo   | 7.62 +- 4.97 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 8620 W: 2079 L: 1890 D: 4651
    Penta | [14, 924, 2239, 1125, 8]
    
    Bench 10214358

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-37.pnn";
+    const char* c_DefaultEvalFile = "eval-41.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
