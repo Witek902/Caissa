0384a82e1570839d1765e16d140913ab82155c2d
Michal Witanowski <witek902@gmail.com>
2024-04-20 22:28:31 +0200

    New net
    Finetuning eval-42 for 40B iterations with 11B positions.
    
    Elo   | 3.84 +- 2.70 (95%)
    SPRT  | N=20000 Threads=1 Hash=16MB
    LLR   | 3.00 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 40082 W: 12841 L: 12398 D: 14843
    Penta | [1218, 4499, 8298, 4674, 1352]
    
    Elo   | 1.56 +- 1.69 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.55 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 77056 W: 18464 L: 18117 D: 40475
    Penta | [269, 9221, 19240, 9490, 308]
    
    Bench 10318582

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-42.pnn";
+    const char* c_DefaultEvalFile = "eval-43.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
