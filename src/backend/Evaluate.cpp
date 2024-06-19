811d694595c0b9c0b2d6b0bb8d57a1e2c9dd2b2b
Michal Witanowski <witek902@gmail.com>
2024-06-19 22:14:17 +0200

    New neural net
    Finetuning previous net with more data.
    Dataset size: 12.47B positions
    First phase: LR=1e-4, 75B iterations
    Second phase: LR=1e-5, 18B iterations
    
    Elo   | 5.19 +- 3.24 (95%)
    SPRT  | N=20000 Threads=1 Hash=8MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 27632 W: 8854 L: 8441 D: 10337
    Penta | [815, 3131, 5620, 3326, 924]
    
    Elo   | 4.60 +- 3.70 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 16300 W: 4031 L: 3815 D: 8454
    Penta | [55, 1889, 4067, 2063, 76]
    
    Elo   | 5.22 +- 5.04 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 1.87 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 8386 W: 1995 L: 1869 D: 4522
    Penta | [12, 927, 2188, 1055, 11]
    
    Bench 8216087

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -23,7 +23,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-43.pnn";
+    const char* c_DefaultEvalFile = "eval-44.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
