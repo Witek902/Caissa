bb6e9dc84db90a6f0720250730cd99ee562fd8ee
Michal Witanowski <witek902@gmail.com>
2024-10-26 09:34:58 +0200

    Search: Remove "deeper"
    Bench 5651232

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -43,8 +43,6 @@ DEFINE_PARAM(LmrCaptureImproving, 26, -128, 256);
 DEFINE_PARAM(LmrCaptureInCheck, 40, -128, 256);
 DEFINE_PARAM(LmrTTHighDepth, 63, -128, 256);
 
-DEFINE_PARAM(LmrDeeperTreshold, 90, 20, 200);
-
 DEFINE_PARAM(ProbcutStartDepth, 5, 3, 8);
 DEFINE_PARAM(ProbcutBetaOffset, 143, 80, 300);
 DEFINE_PARAM(ProbcutBetaOffsetInCheck, 322, 100, 500);
@@ -1962,15 +1960,9 @@ ScoreType Search::NegaMax(ThreadData& thread, NodeInfo* node, SearchContext& ctx
             ASSERT(score >= -CheckmateValue && score <= CheckmateValue);
 
             if (score > alpha)
-            {
-                newDepth += (score > bestValue + LmrDeeperTreshold) && (node->ply < 2 * thread.rootDepth); // prevent search explosions
                 newDepth -= (score < bestValue + newDepth);
-                doFullDepthSearch = newDepth > lmrDepth;
-            }
-            else
-            {
-                doFullDepthSearch = false;
-            }
+
+            doFullDepthSearch = false;
         }
 
         // PVS search at full depth
