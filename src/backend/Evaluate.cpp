fcb529684371f933b3a30d7581e569bbfd1d3717
Michal Witanowski <witek902@gmail.com>
2025-01-06 22:06:52 +0100

    New neural net
    UHO_Lichess_4852_v1.epd:
    Elo   | 0.04 +- 1.16 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | -2.96 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 84112 W: 19222 L: 19212 D: 45678
    Penta | [110, 9834, 22133, 9894, 85]
    
    DFRC.epd:
    Elo   | 0.99 +- 0.66 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 290554 W: 53017 L: 52186 D: 185351
    Penta | [2156, 30223, 79739, 30952, 2207]
    
    Bench 7183857

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -17,7 +17,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-52.pnn";
+    const char* c_DefaultEvalFile = "eval-53.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
