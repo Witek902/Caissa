a24ef7e56b992bf565a64d9976228ab6cee5de6d
Michal Witanowski <witek902@gmail.com>
2025-09-23 20:12:38 +0200

    Bench 7237210

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1074,8 +1074,8 @@ ScoreType Search::QuiescenceNegaMax(ThreadData& thread, NodeInfo* node, SearchCo
 
         if (bestValue >= beta)
         {
-            if (std::abs(bestValue) < KnownWinValue && std::abs(beta) < KnownWinValue)
-                bestValue = (bestValue + beta) / 2;
+            if (bestValue > beta && std::abs(bestValue) < KnownWinValue && std::abs(beta) < KnownWinValue)
+                bestValue = (bestValue + 3 * beta) / 4;
 
             if (!ttEntry.IsValid())
                 ctx.searchParam.transpositionTable.Write(position, ScoreToTT(bestValue, node->ply), node->staticEval, 0, TTEntry::Bounds::Lower);
@@ -1212,8 +1212,8 @@ ScoreType Search::QuiescenceNegaMax(ThreadData& thread, NodeInfo* node, SearchCo
         return -CheckmateValue + (ScoreType)node->ply;
     }
 
-    if (std::abs(bestValue) < KnownWinValue && bestValue > beta)
-        bestValue = (bestValue + beta) / 2;
+    if (bestValue > beta && std::abs(bestValue) < KnownWinValue && std::abs(beta) < KnownWinValue)
+        bestValue = (bestValue + 3 * beta) / 4;
 
     // store value in transposition table
     const TTEntry::Bounds bounds = bestValue >= beta ? TTEntry::Bounds::Lower : TTEntry::Bounds::Upper;
