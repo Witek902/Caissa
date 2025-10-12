ec2ef7fc4083ae8d49edfbf19b2e2098d8c28bae
Michal Witanowski <witek902@gmail.com>
2025-10-12 12:13:46 +0200

    New neural net
    - 16B training position in dataset
    - 78B iterations
    - Batch size = 32K, final LR = 1.25e-6
    
    Elo   | 1.37 +- 1.07 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 2.00]
    Games | N: 109390 W: 26227 L: 25796 D: 57367
    Penta | [423, 12905, 27617, 13318, 432]
    
    Bench 7335325

--- src/backend/Evaluate.cpp
+++ src/backend/Evaluate.cpp
@@ -17,7 +17,7 @@
 #else // !defined(CAISSA_EVALFILE)
 
     // use eval file
-    const char* c_DefaultEvalFile = "eval-64.pnn";
+    const char* c_DefaultEvalFile = "eval-65.pnn";
 
 #endif // defined(CAISSA_EVALFILE)
 
