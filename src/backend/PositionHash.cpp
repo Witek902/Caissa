9179709631cd31f4b234e5ac5429d3823084943a
Michal Witanowski <witek902@gmail.com>
2025-12-30 17:23:41 +0100

    Bench 23552083

--- src/backend/PositionHash.cpp
+++ src/backend/PositionHash.cpp
@@ -9,29 +9,18 @@
 alignas(64) uint64_t s_ZobristHash[c_ZobristHashSize];
 
 
-static inline uint64_t rotl(const uint64_t x, int k)
-{
-    return (x << k) | (x >> (64 - k));
-}
-
-static uint64_t xoroshiro128(uint64_t s[2])
-{
-    // https://prng.di.unimi.it/xoroshiro128plus.c
-    const uint64_t s0 = s[0];
-    uint64_t s1 = s[1];
-    const uint64_t result = s0 + s1;
-    s1 ^= s0;
-    s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
-    s[1] = rotl(s1, 37); // c
-    return result;
-}
-
 void InitZobristHash()
 {
-    uint64_t s[2] = { 0x2b2fa1f53b24b9f2, 0x0203c66609c7f249 };
+    uint64_t s = 0xa7a57e2fba74af2cULL;
 
     for (uint32_t i = 0; i < c_ZobristHashSize; ++i)
     {
-        s_ZobristHash[i] = xoroshiro128(s);
+        // SplitMix64 hash function
+        // https://prng.di.unimi.it/splitmix64.c
+        uint64_t z = (s += 0x9e3779b97f4a7c15ULL);
+        z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
+        z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
+
+        s_ZobristHash[i] = z ^ (z >> 31);
     }
 }
