9aa68eb7527748f5b7f9cb571ceb264b1e7fe839
Michal Witanowski <witek902@gmail.com>
2026-02-06 17:58:36 +0100

    14 king buckets training

--- scripts/visualizeNet.py
+++ scripts/visualizeNet.py
@@ -103,7 +103,7 @@ def main():
     boardViewImg = Image.new('RGB', (imgWidth, imgHeight), color='black')
     boardViewPixels = boardViewImg.load()
 
-    kingBucket = 0
+    kingBucket = 10
 
     for i in range(768):
         for j in range(accumulatorSize):
