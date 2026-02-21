fe77ab9be3cd3562f785b94c01ad19e14b225f89
Michal Witanowski <witek902@gmail.com>
2026-02-21 20:22:01 +0100

    Time manager tweak
    Bench 29912447

--- src/backend/TimeManager.cpp
+++ src/backend/TimeManager.cpp
@@ -7,8 +7,9 @@ DEFINE_PARAM(TM_MovesLeftMidpoint, 35, 30, 60);
 DEFINE_PARAM(TM_MovesLeftSteepness, 219, 150, 260);
 DEFINE_PARAM(TM_IdealTimeFactor, 823, 700, 1000);
 DEFINE_PARAM(TM_MaxTimeFactor, 450, 100, 1000);
-DEFINE_PARAM(TM_NodesCountScale, 208, 160, 260);
-DEFINE_PARAM(TM_NodesCountOffset, 63, 10, 90);
+DEFINE_PARAM(TM_NodesCountScale, 217, 160, 260);
+DEFINE_PARAM(TM_NodesCountOffset, 271, 200, 250);
+DEFINE_PARAM(TM_NodesCountMinFactor, 55, 20, 80);
 DEFINE_PARAM(TM_StabilityScale, 58, 0, 200);
 DEFINE_PARAM(TM_StabilityOffset, 1549, 1000, 2000);
 DEFINE_PARAM(TM_PredictedMoveHitScale, 915, 800, 1000);
@@ -97,10 +98,10 @@ void UpdateTimeManager(const TimeManagerUpdateData& data, SearchLimits& limits, 
 
     // decrease time if nodes fraction spent on best move is high
     {
-        const double nonBestMoveNodeFraction = 1.0 - data.bestMoveNodeFraction;
         const double scale = static_cast<double>(TM_NodesCountScale) / 100.0;
         const double offset = static_cast<double>(TM_NodesCountOffset) / 100.0;
-        const double nodeCountFactor = nonBestMoveNodeFraction * scale + offset;
+        const double minFactor = static_cast<double>(TM_NodesCountMinFactor) / 100.0;
+        const double nodeCountFactor = std::max(offset - data.bestMoveNodeFraction * scale, minFactor);
         limits.idealTimeCurrent *= nodeCountFactor;
     }
 
