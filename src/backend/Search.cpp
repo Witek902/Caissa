b34bad9eac3a4bb52f097b7e927c5d59b9885fcc
Michal Witanowski <witek902@gmail.com>
2026-03-19 20:43:44 +0100

    Bench 7794273

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1187,6 +1187,10 @@ ScoreType Search::QuiescenceNegaMax(ThreadData& thread, NodeInfo* node, SearchCo
 
     while (movePicker.PickMove(*node, move, moveScore))
     {
+        // Move Count Pruning
+        if (bestValue > -TablebaseWinValue && moveIndex > 3)
+            break;
+
         if (bestValue > -TablebaseWinValue && position.HasNonPawnMaterial(position.GetSideToMove()))
         {
             ASSERT(!node->isInCheck);
@@ -1218,16 +1222,6 @@ ScoreType Search::QuiescenceNegaMax(ThreadData& thread, NodeInfo* node, SearchCo
         if (!childNode.position.DoMove(move, childNode.nnContext))
             continue;
         moveIndex++;
-
-        // Move Count Pruning
-        // skip everything after some sane amount of moves has been tried
-        // there shouldn't be many "good" captures available in a "normal" chess positions
-        if (bestValue > -TablebaseWinValue)
-        {
-                 if (node->depth < -4 && moveIndex > 1) break;
-            else if (node->depth < -2 && moveIndex > 2) break;
-            else if (node->depth <  0 && moveIndex > 3) break;
-        }
 
         childNode.previousMove = move;
         childNode.position.ComputeThreats(childNode.threats);
